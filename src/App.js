90303e2b7e465e9c506eaa941cb3ca732e1375b9
Alejandro Vargas <thecore@MacBook-Pro-de-Alejandro.local>
2022-01-27 13:51:43 -0600

    Added Travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hi Mrs Iron Man
+          Edit <code>src/App.js</code> Hi Mrs Iron Man, Superman, and Batman, and mujer maravilla
         </p>
         <a
           className="App-link"
