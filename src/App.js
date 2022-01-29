53efbb59a7346b929f3a6035bb81a5e6d328d4cd
Alex Vargas <thecore@MacBook-Pro-de-Alejandro.local>
2022-01-29 17:22:45 -0600

    Se cambió dockerfile a Dockerfile

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to the AWS, Mr. Stark
+          Edit <code>src/App.js</code> Welcome to the AWS, Mr. Stark, Hulk
         </p>
         <a
           className="App-link"
