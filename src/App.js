f5454da2b95fb25bdeb2ef991013085ccb6e8a99
Alex Vargas <thecore@MacBook-Pro-de-Alejandro.local>
2022-03-10 22:26:56 -0600

    Se modifico apps.js con devops azure V2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Prueba con jenkins y devops azure
+          Edit <code>src/App.js</code> Prueba con jenkins y devops azure v2
         </p>
         <a
           className="App-link"
